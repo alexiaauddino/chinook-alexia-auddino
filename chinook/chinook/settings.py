1bb7f4d4b6e5d37ca845784afee0c6091b0284f8
Alexia Auddino <alexia.auddino@student.isae-supaero.fr>
2022-04-11 22:25:08 +0200

    Ajout base de donnees chinook

--- chinook/chinook/settings.py
+++ chinook/chinook/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'chinook.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'chinook.db',
     }
 }
 
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
